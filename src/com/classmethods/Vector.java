e4da1a54ffaa6076831204363824a8d1ac31c6e4
Pedro Vidaurri <vidaurribk@yahoo.com>
2021-10-07 10:49:10 -0700

    first commit

---
+++ src/com/classmethods/Vector.java
@@ -0,0 +1,36 @@
+public class Vector {
+    private float x;
+    private float y;
+
+    public void add(Vector other) {
+
+    }
+    public void sub(Vector other) {
+
+    }
+    public void mult(float value) {
+
+    }
+    public Vector plus(Vector other) {
+
+    }
+    return null;
+    public Vector minus(Vector other) {
+
+    }
+    public Vector times(Vector other) {
+
+    }
+    public float magnitude() {
+
+    }
+    public float magSq() {
+
+    }
+    public void normalized() {
+
+    }
+    public Vector normalized() {
+
+    }
+}
